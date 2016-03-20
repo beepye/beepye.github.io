b66823d08440172605d1434303a8c494e273a659
Brian Pye <bpye@constantcontact.com>
2016-03-20 08:27:50 -0400

    Initializing repo with necessary files

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+  module.exports = function(grunt) {
+
+    'use strict';
+
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    //Initialize tasks
+    grunt.initConfig({
+
+      pkg: grunt.file.readJSON('package.json'),
+
+      rootPath:   './',
+      cssPath:    '<%= rootPath %>/styles/css/',
+      scssPath:   '<%= rootPath %>/styles/scss/',
+
+      sass: {
+        dist: { 
+          files: [{
+            expand: true,
+            cwd: '<%= scssPath %>',
+            src: [ '**/*.scss' ],
+            dest: '<%= cssPath %>',
+            ext: '.css'
+          }]
+        }
+      }, // scss
+
+      watch: {
+        scss: {
+          files: [ '**/*.scss' ],
+          tasks: [ 'sass:dist' ],
+          options: { livereload: true }
+        }
+      }, // watch
+
+      connect: {
+        source: {
+          options: {
+            port: 8080,
+            keepalive: true,
+            base: '<%= rootPath %>'
+          }
+        }
+      } // server
+
+    });
+
+    grunt.registerTask( 'sandbox', [ 'connect:source' ] );
+
+    grunt.registerTask('dev', [ 'watch' ] );
+    
+};
