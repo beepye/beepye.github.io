1948037a825ca7f4c95faded29214143e1d04fad
Brian Pye <bpye@constantcontact.com>
2016-03-20 15:55:08 -0400

    Adding touches to styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-  module.exports = function(grunt) {
+module.exports = function(grunt) {
 
     'use strict';
 
@@ -7,46 +7,49 @@
     //Initialize tasks
     grunt.initConfig({
 
-      pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON('package.json'),
 
-      rootPath:   './',
-      cssPath:    '<%= rootPath %>/styles/css/',
-      scssPath:   '<%= rootPath %>/styles/scss/',
+        rootPath:   './',
+        cssPath:    '<%= rootPath %>/styles/css/',
+        scssPath:   '<%= rootPath %>/styles/scss/',
 
-      sass: {
-        dist: { 
-          files: [{
-            expand: true,
-            cwd: '<%= scssPath %>',
-            src: [ '**/*.scss' ],
-            dest: '<%= cssPath %>',
-            ext: '.css'
-          }]
-        }
-      }, // scss
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: [{
+                    expand: true,
+                    cwd:    '<%= scssPath %>',
+                    src:    [ 'styles.scss' ],
+                    dest:   '<%= cssPath %>',
+                    ext:    '.css'
+                }]
+            }
+        }, // scss
 
-      watch: {
-        scss: {
-          files: [ '**/*.scss' ],
-          tasks: [ 'sass:dist' ],
-          options: { livereload: true }
-        }
-      }, // watch
+        watch: {
+            scss: {
+                files:  [ '**/*.scss' ],
+                tasks:  [ 'sass:dist' ],
+                options:{ livereload: true }
+            }
+        }, // watch
 
-      connect: {
-        source: {
-          options: {
-            port: 8080,
-            keepalive: true,
-            base: '<%= rootPath %>'
-          }
-        }
-      } // server
+        connect: {
+            source: {
+                options: {
+                    port:       8080,
+                    keepalive:  true,
+                    base:       '<%= rootPath %>'
+                }
+            }
+        } // server
 
     });
 
     grunt.registerTask( 'sandbox', [ 'connect:source' ] );
 
     grunt.registerTask('dev', [ 'watch' ] );
-    
+
 };
