9836110fdffabf3cdd839861bc2190afc4457ba1
Brian Pye <5097038+beepye@users.noreply.github.com>
2019-01-17 09:09:03 -0500

    adjusting hero animation timing

--- script/site.js
+++ script/site.js
@@ -2,5 +2,5 @@ $(function(){
 
 	setTimeout(function(){ $('.hero-headerContainer').addClass('pulse'); }, 500); 
 
-	setTimeout(function(){ $('.hero-iconContainer').addClass('pulse'); }, 1175); 
+	setTimeout(function(){ $('.hero-iconContainer').addClass('pulse slower'); }, 900); 
 })
