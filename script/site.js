d60d73fbc1db7a80c3c242d4dcfefa148a2900e3
James Thaumiel <starchturrets@protonmail.com>
2019-06-18 10:51:21 +0400

    Rewrote the animation js without jquery

--- script/site.js
+++ script/site.js
@@ -1,6 +1,12 @@
-$(function(){
+/* eslint-disable func-names */
+// eslint-disable-next-line strict
+(() => {
+  setTimeout(() => {
+    document.querySelector('.hero-headerContainer').classList.add('pulse');
+  }, 500);
 
-	setTimeout(function(){ $('.hero-headerContainer').addClass('pulse'); }, 500); 
-
-	setTimeout(function(){ $('.hero-iconContainer').addClass('pulse slower'); }, 900); 
-})
+  setTimeout(() => {
+    document.querySelector('.hero-iconContainer').classList.add('pulse');
+    document.querySelector('.hero-iconContainer').classList.add('slower');
+  }, 900);
+})();
